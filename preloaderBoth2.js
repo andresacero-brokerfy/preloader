0795ba7de52c66f3599c646e5d85a8a17e78c45f
andresacero-brokerfy <148167109+andresacero-brokerfy@users.noreply.github.com>
2023-12-03 11:22:00 -0600

    Create preloaderBoth2.js

---
+++ preloaderBoth2.js
@@ -0,0 +1,47 @@
+// Create the overlay and SVG elements
+const overlay = document.createElement('div');
+overlay.id = 'loader';
+overlay.style.cssText = `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.7);
+    z-index: 999;
+`;
+
+const svgImage = document.createElement('img');
+svgImage.id = 'svgImage';
+svgImage.src = 'https://cdn.jsdelivr.net/gh/andresacero-brokerfy/preloader@c2f73a6973fd1fd85c37b1b18e667d8e503e7858/svg_combined_2.svg';
+svgImage.style.cssText = `
+    width: 120px;  // Ancho fijo de 120px
+    height: auto;  // Alto se ajusta automáticamente
+    display: none;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
+
+// Append the elements to the body
+document.body.appendChild(overlay);
+document.body.appendChild(svgImage);
+
+// Function to hide the overlay and display the SVG
+function hideOverlay() {
+    overlay.style.display = 'none';
+    svgImage.style.display = 'block';
+}
+
+// Add an event listener to hide the overlay when all external JS files are loaded
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(hideOverlay, 100);
+});
+
+// Fallback: If all external resources are loaded and the DOMContentLoaded event doesn't fire,
+// we'll still hide the overlay when the window's load event is triggered.
+window.addEventListener('load', hideOverlay);
